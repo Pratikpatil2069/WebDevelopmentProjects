974563c88dcd66f63f62cc4fe728d31b2d6f8d29
Pratik Patil <pratikpatil2069@gmail.com>
2025-07-20 01:02:02 +0530

    Initial commit

---
+++ First.js
@@ -0,0 +1,53 @@
+/*console.log("welcome to javaScript!");
+
+
+let name="pratik";
+let age=21;
+let a=null;
+const b=undefined;
+const flag=true;
+console.log(name);
+console.log(age);
+console.log(a);
+console.log(b);
+console.log(flag);
+
+
+const student={
+    name:"patil",
+    age:20,
+    marks:100.0,
+    isPass:true,
+};
+console.log(student.name);
+console.log(student.age);
+student[name]="Pratik_patil";
+console.log(student[name]);
+
+
+// this is a single line comment.
+/*this is a multiple 
+line code .*/
+
+// operators.
+
+//conditional statements.
+
+// loops.
+
+//let FullName=prompt("enter the name:");
+//console.log(FullName);
+
+
+//alert("message");
+
+
+/*let str="yrgfrvhqrf";
+for(let i of str){
+    console.log(i);
+}
+
+for(let key in student){
+    console.log(student[key]);
+}*/
+
