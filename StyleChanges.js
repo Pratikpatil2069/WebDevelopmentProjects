b00b7116bef872348e4e0e46602edfd8ab3108c6
Pratik Patil <pratikpatil2069@gmail.com>
2025-07-21 09:33:37 +0530

    add StyleChanges.css, StyleChanges.html and  StyleChanges.js

---
+++ StyleChanges.js
@@ -0,0 +1,11 @@
+let div=document.querySelector("div");
+console.log(div);
+
+//accessing div style.
+console.log(div.style);
+
+//modifying style using js.
+
+div.style.backgroundColor="green";
+div.style.fontSize="20px";
+div.innerText="patil";
