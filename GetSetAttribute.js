372e1b2a6cce859b70fee4036b26ea1cba7c11e5
Pratik Patil <pratikpatil2069@gmail.com>
2025-07-21 09:11:05 +0530

    add GetSetAttribute.html and GetSetAttribute.js

---
+++ GetSetAttribute.js
@@ -0,0 +1,12 @@
+// accessing element . 
+let name=document.querySelector("p");
+console.log(name);
+
+// accessing attribute of element
+let attr=name.getAttribute("id");
+console.log(attr);
+
+// to set attribute to the elemet
+let div=document.querySelector("div");
+console.log(div.setAttribute("class","second"));
+
